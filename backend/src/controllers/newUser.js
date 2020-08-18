bf21358f73ed7b2c5edc26a50243e3e2bedc6eb6
mitcmurph <mitcmurph@gmail.com>
2020-08-18 21:33:36 +1000

    Implement standard api responses to newUser endpoint

--- backend/src/controllers/newUser.js
+++ backend/src/controllers/newUser.js
@@ -1,6 +1,10 @@
 import firestore from "../helpers/firestore";
 import admin from "../helpers/firebase-admin";
 import { v4 as uuidv4 } from "uuid";
+import {
+    successResponse,
+    handleApiError
+} from "../helpers/apiResponse";
 
 export const newUser = async (req, res) => {
     try {
@@ -8,24 +12,14 @@ export const newUser = async (req, res) => {
 
         //Check if fields are completed
         if (email.length === 0 || password.length === 0) {
-            console.log("field bad")
             throw "Undefined fields"
         }
 
         const user = await admin.auth().createUser({ email, password })
-        return res.status(200).json({
-            result: "Success",
-            msg: "User Created",
-            user: user
-        }
+        return res.status(200).json(
+            successResponse(user)
         );
     } catch (error) {
-        console.log(error)
-        return res.status(400).json({
-            result: "Failed",
-            msg: "User not Created",
-            error: error
-        }
-        );
+        return handleApiError(res, error);
     }
 };
