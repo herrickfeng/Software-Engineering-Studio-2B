5b2015f61cabb728a3cf888c148460617008a7a3
Agid-Kaharuba <razinagida@gmail.com>
2020-08-22 18:31:04 +1000

    Improved class list with listeners and displaying data

--- frontend/src/components/classList/index.js
+++ frontend/src/components/classList/index.js
@@ -7,6 +7,8 @@ import {Link} from "react-router-dom";
  * An example for what can be passed in via props
  * {
  *   data: // Data containing all the classes which will be displayed
+ *   onAddClick: // A listener for when add class is clicked
+ *   onBackClick: // A listener for when back is clicked
  *   addPath: // Path to another route via react router
  *   backPath: // Path to previous route via react router
  * }
@@ -15,10 +17,39 @@ import {Link} from "react-router-dom";
  */
 export default function ClassList(props) {
 
-  /* TODO there should be some way to pass the data that is fetched from the backend into the props of this component.
-   * We should process that here, and pass it into table rows. Hopefully we can do this as soon as we know what the data
-   * from the backend will look like which will be soon.
-   */
+  // TODO there should be some way to pass the data that is fetched from the backend into the props of this component.
+  // We can then pass it into table rows as shown below.
+
+  let sampleData = [
+    {
+      name: "Week 1",
+      date: "11/8/2020",
+      startTime: "12:00",
+      endTime: "14:00",
+      classCode: 123123,
+    },
+    {
+      name: "Week 2",
+      date: "18/8/2020",
+      startTime: "12:00",
+      endTime: "14:00",
+      classCode: 392044,
+    },
+    {
+      name: "Week 3",
+      date: "25/8/2020",
+      startTime: "12:00",
+      endTime: "14:00",
+      classCode: 320940,
+    },
+    {
+      name: "Week 4",
+      date: "1/1/2020",
+      startTime: "12:00",
+      endTime: "14:00",
+      classCode: 129032,
+    },
+  ];
 
   return (
     <>
@@ -33,22 +64,22 @@ export default function ClassList(props) {
         </TableHead>
 
         <TableBody>
-          {new Array(10).fill(
+          {sampleData.map(entry => (
             <TableRow>
-              <TableCell>Bob</TableCell>
-              <TableCell>2020/8/20</TableCell>
-              <TableCell>14:00</TableCell>
-              <TableCell>1293491</TableCell>
-            </TableRow>)}
-
+              <TableCell>{entry.name}</TableCell>
+              <TableCell>{entry.date}</TableCell>
+              <TableCell>{entry.startTime}-{entry.endTime}</TableCell>
+              <TableCell>{entry.classCode}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </LinedTable>
 
       <Box my={3} display={"flex"} justifyContent={"space-between"}>
-        <Button variant={"outlined"} color={"primary"} component={Link} to={props.backPath}>
+        <Button variant={"outlined"} color={"primary"} component={Link} to={props.backPath} onClick={props.onBackClick}>
           Back
         </Button>
-        <Button variant={"outlined"} color={"primary"} component={Link} to={props.addPath}>
+        <Button variant={"outlined"} color={"primary"} component={Link} to={props.addPath} onClick={props.onAddClick}>
           + Add Class
         </Button>
       </Box>
