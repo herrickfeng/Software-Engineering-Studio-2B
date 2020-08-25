714b89c29f4f784b5565049db2220ffb88be4af7
Calvin Dong <calvin.dong156@gmail.com>
2020-08-25 16:22:10 +1000

    - Added upload capability

--- frontend/src/pages/upload/index.js
+++ frontend/src/pages/upload/index.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { useState } from 'react';
 import Button from "@material-ui/core/Button";
-import { TextField, makeStyles } from "@material-ui/core";
+import Input from '@material-ui/core/Input';
 import Grid from "@material-ui/core/Grid";
+import { store } from "../../helpers/firebase"
 
 const centreStyle = {
     display: "flex",
@@ -14,22 +16,59 @@ const centreStyle = {
     flexDirection: "column"
 };
 
-const handleUpload = async (event) => {
-    console.log("up")
-    console.log(event.target.files[0])
-}
+export default function UploadImageForm() {
+   const [progressValue, setProgressValue] = useState(0);
 
-export default function uploadImageForm(props) {
+    const handleUpload = async (event) => {
+        console.log("up")
+        var file = event.target.files[0]
+        console.log(file)
+        console.log(store)
+        var storageRef = store().ref(file.name)
+        var task = storageRef.put(file)
+
+        task.on('state_changed',
+            function (snapshot) {
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                //var uploader = document.getElementById("uploader")
+                setProgressValue(percentage)
+            },
+            function error(err) {
+                switch (err.code) {
+                    case 'storage/unauthorized':
+                        console.log("User doesn't have permission to access the object")
+                        break;
+
+                    case 'storage/canceled':
+                        console.log("User canceled the upload")
+                        break;
+
+                    case 'storage/unknown':
+                        console.log("Unknown error occurred, inspect error.serverResponse")
+                        break;
+
+                    case 'storage/unauthenticated':
+                        console.log("User is not authenticated")
+                        break;
+                }
+            },
+            function (complete) {
+                console.log("Uploaded!")
+            }
+        )
+    }
+
     return (
         <Grid container justifyContent="center" direction="row">
             <Grid item xs={12}>
                 <div>
                     <h1> heya there </h1>
                     <h1> heya there </h1>
-                    <input type="file" onChange={handleUpload}/>
+                    <Input type="file" onChange={handleUpload} />
                 </div>
             </Grid>
-            <Grid>
+            <Grid item>
+                <progress value={progressValue} max="100" id="uploader"> 0% </progress>
                 <h1> PLEASE </h1>
             </Grid>
         </Grid>
