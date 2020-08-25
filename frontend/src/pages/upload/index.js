e6ff00e3c3d2cb135b8e40a99da6aecdc016c9a1
Calvin Dong <calvin.dong156@gmail.com>
2020-08-25 23:34:01 +1000

    - added progress messages

--- frontend/src/pages/upload/index.js
+++ frontend/src/pages/upload/index.js
@@ -1,75 +1,68 @@
 import React from "react";
-import { useState } from 'react';
-import Button from "@material-ui/core/Button";
 import Input from '@material-ui/core/Input';
 import Grid from "@material-ui/core/Grid";
 import { store } from "../../helpers/firebase"
 
-const centreStyle = {
-    display: "flex",
-    minHeight: "100vh",
-    width: "100%",
-    padding: "0",
-    margin: "0",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
-};
-
 export default function UploadImageForm() {
-   const [progressValue, setProgressValue] = useState(0);
+    const [progressValue, setProgressValue] = React.useState(0)
+    const [showText, setShowField] = React.useState(false)
+    const [showResult, setShowResult] = React.useState("")
 
     const handleUpload = async (event) => {
-        console.log("up")
-        var file = event.target.files[0]
-        console.log(file)
-        console.log(store)
-        var storageRef = store().ref(file.name)
-        var task = storageRef.put(file)
+        try {
+            setShowField(false)
+            setProgressValue(0)
+            var file = event.target.files[0]
+            var storageRef = store().ref(file.name)
+            //var metadata = { contentType: 'image/jpeg' };
+            var task = storageRef.put(file)
 
-        task.on('state_changed',
-            function (snapshot) {
-                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                //var uploader = document.getElementById("uploader")
-                setProgressValue(percentage)
-            },
-            function error(err) {
-                switch (err.code) {
-                    case 'storage/unauthorized':
-                        console.log("User doesn't have permission to access the object")
-                        break;
+            task.on('state_changed',
+                function (snapshot) {
+                    var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    setProgressValue(percentage)
+                },
+                function error(err) {
+                    setShowResult("Something went wrong :(")
+                    setShowField(true)
+                    switch (err.code) {
+                        case 'storage/unauthorized':
+                            console.log("User doesn't have permission to access the object")
+                            break;
 
-                    case 'storage/canceled':
-                        console.log("User canceled the upload")
-                        break;
+                        case 'storage/canceled':
+                            console.log("User canceled the upload")
+                            break;
 
-                    case 'storage/unknown':
-                        console.log("Unknown error occurred, inspect error.serverResponse")
-                        break;
+                        case 'storage/unknown':
+                            console.log("Unknown error occurred, inspect error.serverResponse")
+                            break;
 
-                    case 'storage/unauthenticated':
-                        console.log("User is not authenticated")
-                        break;
+                        case 'storage/unauthenticated':
+                            console.log("User is not authenticated")
+                            break;
+                    }
+                },
+                function (complete) {
+                    setShowResult("Image Uploaded!")
+                    setShowField(true)
+                    console.log("Uploaded!")
                 }
-            },
-            function (complete) {
-                console.log("Uploaded!")
-            }
-        )
+            )
+        }
+        catch (error) {
+            console.log("did an oopsie")
+        }
     }
 
     return (
-        <Grid container justifyContent="center" direction="row">
-            <Grid item xs={12}>
-                <div>
-                    <h1> heya there </h1>
-                    <h1> heya there </h1>
-                    <Input type="file" onChange={handleUpload} />
-                </div>
+        <Grid container>
+            <Grid item xs={12} justifyContent="center">
+                <Input type="file" onChange={handleUpload} />
             </Grid>
             <Grid item>
-                <progress value={progressValue} max="100" id="uploader"> 0% </progress>
-                <h1> PLEASE </h1>
+                <progress value={progressValue} max="100" />
+                {showText ? <b>{showResult}</b> : null}
             </Grid>
         </Grid>
         );
