a651e573bc3ec80309bba07da9c3a481e09b2e52
mitcmurph <mitcmurph@gmail.com>
2020-09-05 18:27:46 +1000

    user roles and admin endpoint

--- backend/src/routes/user-router.js
+++ backend/src/routes/user-router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { newUser } from "../controllers/user";
+import { newUser, getUser } from "../controllers/user";
+import { checkToken } from "../middleware/auth";
 
 const userRouter = Router();
 
 userRouter.post("/create", newUser);
+userRouter.get("/", checkToken, getUser);
+userRouter.get("/:userId", getUser);
 
 export default userRouter;
