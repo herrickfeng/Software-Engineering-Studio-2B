7b5e3a3c1e0d62ca0fa53adfe9c4af5c993a2989
mitcmurph <mitcmurph@gmail.com>
2020-09-07 00:24:13 +1000

    implement seeder

---
+++ backend/src/seed/index.seed.js
@@ -0,0 +1,67 @@
+import request from "supertest";
+import teachers from "./teachers.json"
+import students from "./students.json"
+import subjects from "./subjects.json"
+
+function rnd(max) {
+  return Math.floor(Math.random() * max)
+}
+
+describe("seed", () => {
+  var server;
+  var teacherId;
+
+  beforeEach(() => {
+    server = require("../index");
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  it("create teacher", async function () {
+    this.timeout(10000)
+    teacherId = rnd(teachers.length);
+    var teacherBody = teachers[teacherId]
+    teacherBody = {
+      ...teacherBody,
+      test: "teacher"
+    }
+    const response = await request(server).post("/user/create").send(teacherBody);
+  });
+
+  it("login teachers", async function () {
+    this.timeout(10000)
+    var teacherBody = teachers[teacherId]
+    const response = await request(server).post("/auth/login").send(teacherBody);
+    teachers[teacherId] = { ...teacherBody, ...response.body.data }
+  });
+
+  it("create subjects", async function () {
+    this.timeout(10000)
+    var teacherBody = teachers[teacherId]
+    for (var i = 0; i < subjects.length; i++) {
+      var subjectBody = subjects[i];
+
+      const response = await request(server).post("/admin/subject/create").set("Authorization", `Bearer ${teacherBody.idToken}`).send(subjectBody);
+      subjectBody = { ...subjectBody, ...response.body.data }
+    }
+  });
+
+  it("create and enroll students subject", async function () {
+    this.timeout(60000)
+    for (var i = 0; i < students.length; i++) {
+      var studentBody = students[i];
+
+      var response;
+      response = await request(server).post("/user/create").send(studentBody);
+      response = await request(server).post("/auth/login").send(studentBody);
+      studentBody = { ...studentBody, ...response.body.data }
+
+      for (var j = 0; j < subjects.length; j++) {
+        var subjectBody = subjects[j]
+        await request(server).post("/subject/join").set("Authorization", `Bearer ${studentBody.idToken}`).send(subjectBody);
+      }
+    }
+  });
+});
