c3eb10244c96b1b1a4bf724435798963a70af546
mitcmurph <mitcmurph@gmail.com>
2020-09-08 16:37:28 +1000

    this file was half-tabs half-2spaceGame me the shits

--- frontend/src/helpers/api/index.js
+++ frontend/src/helpers/api/index.js
@@ -1,28 +1,28 @@
 import { createUser } from "./user";
 import { uploadImage } from "./upload";
 import {
-    createSubjectAdmin,
-    getAllSubjectAdmin,
-    getSubjectAdmin,
-    updateSubjectAdmin,
-    deleteSubjectAdmin
+  createSubjectAdmin,
+  getAllSubjectAdmin,
+  getSubjectAdmin,
+  updateSubjectAdmin,
+  deleteSubjectAdmin
 } from "./subject";
 
 export const API_HOST = "localhost:4000";
 
 export default {
-    user: {
-      create: createUser,
-      upload: uploadImage,
+  user: {
+    create: createUser,
+    upload: uploadImage,
 
-    },
-    admin: {
-        subject: {
-            create: createSubjectAdmin,
-            getAll: getAllSubjectAdmin,
-            get: getSubjectAdmin,
-            update: updateSubjectAdmin,
-            delete: deleteSubjectAdmin
-        }
+  },
+  admin: {
+    subject: {
+      create: createSubjectAdmin,
+      getAll: getAllSubjectAdmin,
+      get: getSubjectAdmin,
+      update: updateSubjectAdmin,
+      delete: deleteSubjectAdmin
     }
+  }
 }
