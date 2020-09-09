9f2faf0f1f965151e195343e6719db02e4163f1a
mitcmurph <mitcmurph@gmail.com>
2020-09-10 00:25:44 +1000

    student class endpoints

--- backend/src/routes/subject-router.js
+++ backend/src/routes/subject-router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getSubject, getAllStudentSubject, joinSubject } from "../controllers/subject";
+import { getClass, getAllClass } from "../controllers/class";
 import { checkToken } from "../middleware/auth";
 
 const subjectRouter = Router().use(checkToken);
@@ -7,5 +8,9 @@ const subjectRouter = Router().use(checkToken);
 subjectRouter.get("/:id", getSubject);
 subjectRouter.get("/", getAllStudentSubject);
 subjectRouter.post("/join", joinSubject);
+
+//For Class
+subjectRouter.get("/:subjectId/class/:classId", getClass);
+subjectRouter.get("/:subjectId/class", getAllClass);
 
 export default subjectRouter;
