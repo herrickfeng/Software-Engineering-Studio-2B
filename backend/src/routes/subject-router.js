425baad1e875150456ff802caf9565c51397373c
mitcmurph <mitcmurph@gmail.com>
2020-09-25 00:34:06 +1000

    update specific attendance field

--- backend/src/routes/subject-router.js
+++ backend/src/routes/subject-router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createAttendance, getAttendance, getAttendanceBySubClass, getAttendanceBySubStu, updateAttendance } from "../controllers/attendance";
+import { createAttendance, getAttendance, getAttendanceBySubClass, getAttendanceBySubStu, updateAttendance, updateSpecific } from "../controllers/attendance";
 import { getSubject, getAllStudentSubject, joinSubject, getAllStudents } from "../controllers/subject";
 import { getClass, getAllClass } from "../controllers/class";
 import { checkToken } from "../middleware/auth";
@@ -21,6 +21,7 @@ subjectRouter.get("/:subjectId/class/:classId/attendance", getAttendanceBySubCla
 subjectRouter.get("/:subjectId/user/:userId/attendance", getAttendanceBySubStu);
 
 subjectRouter.put("/:subjectId/class/:classId/user/:userId/attendance", updateAttendance);
+subjectRouter.put("/:subjectId/class/:classId/user/:userId/attendance/:attendanceType", updateSpecific);
 //Get all students from subject
 subjectRouter.get("/:subjectId", getAllStudents);
 
