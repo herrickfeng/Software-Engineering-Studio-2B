02efc4303449ca2a44e01e7b975f6ed6891c2b66
Calvin Dong <calvin.dong156@gmail.com>
2020-09-24 13:02:28 +1000

    - Added some colours

--- frontend/src/components/classRoll/index.js
+++ frontend/src/components/classRoll/index.js
@@ -4,11 +4,23 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { makeStyles }from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import React from "react"
 import { createRef, useEffect, useState } from "react"
 
+const useStyles = makeStyles({
+  attending: {
+    backgroundColor: '#90FF9A',
+    //opacity: '0.3'
+    //Yellow: FDFF90
+    //Red: FF9790
+  },
+
+})
+
 export default function ClassAttendanceList(props) {
+  const classes = useStyles();
   let classListData = props.classList
 
   /*
@@ -32,7 +44,7 @@ export default function ClassAttendanceList(props) {
           </TableHead>
           <TableBody>
             {classListData.map(student => (
-              <TableRow>
+              <TableRow className={classes.attending}>
                 <TableCell> {student._label.split("/")[0]} </TableCell>
                 <TableCell> {student._label.split("/")[1]} </TableCell>
               </TableRow>
