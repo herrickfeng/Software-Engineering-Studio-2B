7fe9db4e8f8119c77a6be4c39bb9c9287fcafd2a
mitcmurph <mitcmurph@gmail.com>
2020-10-20 23:31:21 +1100

    verify endpoint

--- backend/src/routes/auth-router.js
+++ backend/src/routes/auth-router.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { login, passwordReset } from "../controllers/auth";
+import { checkToken } from "../middleware/auth";
+import { successResponse } from "../helpers/apiResponse";
 
 const authRouter = Router();
 
 authRouter.post("/login", login);
 authRouter.post("/reset", passwordReset);
+authRouter.get("/verify", checkToken, successResponse);
 
 export default authRouter;
